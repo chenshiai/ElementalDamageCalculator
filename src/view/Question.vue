2b58f0016a507437758cd4d9242cfcfd6fb89300
chenshuai <chenshuai@youzan.com>
2021-03-10 11:50:28 +0800

    feat: 说明更新

--- src/view/Question.vue
+++ src/view/Question.vue
@@ -10,6 +10,7 @@
       </a>
     </p>
   </div>
+  <br />
   <span class="question-title">敌人抗性是多少</span>
   <div>
     <img src="../assets/IMG_1457.png" class="question-img" @click="imagePreview"/>
@@ -20,7 +21,18 @@
       </a>
     </p>
   </div>
-
+  <br />
+  <span class="question-title">误差来自哪里</span>
+  <div>
+    人物属性、武器属性、圣遗物属性、技能倍率等数值，在游戏内实际上是精确到后两位小数的。
+    举个例子，面板显示的「攻击力+20」，实际可能是「攻击力+20.25」。
+  </div>
+  <br />
+  <span class="question-title">滑块数值达到上限</span>
+  <div>
+    滑块只是用来辅助调整到大概数值，而实际数值没有上限，超过滑块上限的数值需要手动输入。
+  </div>
+  <br />
   <span class="question-title">基础伤害数值来源</span>
   <div>
     超载、感电、超导、扩散、碎冰能打多少伤害——鈴@米游社【空荧酒馆】
@@ -31,6 +43,7 @@
       </a>
     </p>
   </div>
+  <br />
   <span class="question-title">伤害计算的公式</span>
   <div>
     《伤害构成论》原神伤害是怎么算的——白猫大人@米游社
