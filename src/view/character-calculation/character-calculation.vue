3770d2b5387bdf78c1b735e02a1f259a25ba4ec5
chenshuai <1183942503@qq.com>
2024-09-10 10:44:23 +0800

    [new]: fock enka上的数据

--- src/view/character-calculation/character-calculation.vue
+++ src/view/character-calculation/character-calculation.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <img src="https://enka.network/ui/UI_EquipIcon_Sword_Dawn_Awaken.png" alt="">
+  <img src="https://enka.network/ui/UI_Talent_S_Chiori_03.png" alt="">
+  <img src="https://enka.network/ui/UI_Talent_U_Chiori_01.png" alt="">
+  <img src="https://enka.network/ui/UI_Talent_S_Chiori_02.png" alt="">
+  <img src="https://enka.network/ui/UI_Talent_U_Chiori_02.png" alt="">
+  <img src="https://enka.network/ui/UI_Talent_S_Chiori_04.png" alt="">
+  <img src="https://enka.network/ui/UI_RelicIcon_15021_4.png" alt=""></img>
 </template>
 
 <script setup lang="ts">
