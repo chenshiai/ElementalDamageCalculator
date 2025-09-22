705b9cfb10a210b4dec5a958073e43314040506d
chenshuai <1183942503@qq.com>
2025-09-22 09:08:28 +0800

    fix：样式调整

--- src/view/moon-electro/index.vue
+++ src/view/moon-electro/index.vue
@@ -484,7 +484,7 @@ const damageResult = computed(() => {
 }
 .element-img {
   display: flex;
-  width: 44px;
+  width: 46px;
   height: 44px;
   border-radius: 50%;
   border: solid 2px var(--border);
